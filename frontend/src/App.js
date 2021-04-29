21af299167637ec973d785920b1a7166b39a152f
Danny Tang <tang_daniel@outlook.com>
2021-04-28 22:11:03 -0700

    configured for merged backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 function App() {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 }
